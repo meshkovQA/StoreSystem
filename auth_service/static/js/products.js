e316bde02f678fd51707194a06915e190bb6e289
alekslynx <siriusmc@mail.ru>
2025-09-19 18:16:35 +0200

    fixed bugs

--- auth_service/static/js/products.js
+++ auth_service/static/js/products.js
@@ -78,12 +78,9 @@ document.addEventListener("DOMContentLoaded", async function () {
         // Вес продукта
         let weight = document.getElementById("add-weight").value;
         weight = weight.replace(',', '.'); // Заменяем запятую на точку
-        if (weight) {
-            if (!/^\d{1,4}(\.\d{1,2})?$/.test(weight) || parseFloat(weight) <= 0 || parseFloat(price) > 1000000 || !/^\d{1,7}\.\d{2}$/.test(price)) {
-                console.log("Invalid weight input");  // Лог ошибки веса
-                document.getElementById("weightError").style.display = 'block';
-                valid = false;
-            }
+        if (!/^\d{1,4}(\.\d{1,2})?$/.test(weight) || parseFloat(weight) <= 0) {
+            document.getElementById("weightError").style.display = 'block';
+            valid = false;
         }
 
         // Габариты
@@ -179,12 +176,9 @@ document.addEventListener("DOMContentLoaded", async function () {
         // Вес продукта
         let weight = document.getElementById("edit-weight").value;
         weight = weight.replace(',', '.'); // Заменяем запятую на точку
-        if (weight) {
-            if (!/^\d{1,4}(\.\d{1,2})?$/.test(weight) || parseFloat(weight) <= 0 || parseFloat(price) > 1000000 || !/^\d{1,7}\.\d{2}$/.test(price)) {
-                console.log("Invalid weight input");  // Лог ошибки веса
-                document.getElementById("editweightError").style.display = 'block';
-                valid = false;
-            }
+        if (!/^\d{1,4}(\.\d{1,2})?$/.test(weight) || parseFloat(weight) <= 0) {
+            document.getElementById("editweightError").style.display = 'block';
+            valid = false;
         }
 
         // Габариты
